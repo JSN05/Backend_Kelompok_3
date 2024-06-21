11ed1f90c9c9f33261a7d1c5faa7980bc7c359fa
dimashadi <dimashadianto460@gmail.com>
2024-06-21 13:18:31 +0700

    Ubah type dan id dalam forgot password dan remove migration

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -24,32 +24,32 @@
 
         <!-- New Password -->
         <div class="mt-4 relative">
-            <x-input-label for="new_password" :value="__('New Password')" />
+            <x-input-label for="password" :value="__('New Password')" />
             <div class="relative">
-                <x-text-input id="new_password" class="block mt-1 w-full pr-10" type="password" name="new_password" required />
-                <button type="button" onclick="togglePasswordVisibility('new_password')" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" tabindex="-1">
-                    <svg id="new_password-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                <x-text-input id="password" class="block mt-1 w-full pr-10" type="password" name="password" required />
+                <button type="button" onclick="togglePasswordVisibility('password')" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" tabindex="-1">
+                    <svg id="password-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
                         <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
                     </svg>
                 </button>
             </div>
-            <x-input-error :messages="$errors->get('new_password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm New Password -->
         <div class="mt-4 relative">
-            <x-input-label for="new_password_confirmation" :value="__('Confirm New Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirm New Password')" />
             <div class="relative">
-                <x-text-input id="new_password_confirmation" class="block mt-1 w-full pr-10" type="password" name="new_password_confirmation" required />
-                <button type="button" onclick="togglePasswordVisibility('new_password_confirmation')" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" tabindex="-1">
-                    <svg id="new_password_confirmation-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                <x-text-input id="password_confirmation" class="block mt-1 w-full pr-10" type="password" name="password_confirmation" required />
+                <button type="button" onclick="togglePasswordVisibility('password_confirmation')" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" tabindex="-1">
+                    <svg id="password_confirmation-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
                         <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
                     </svg>
                 </button>
             </div>
-            <x-input-error :messages="$errors->get('new_password_confirmation')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
