fa383712c769928f869808adfc093acb356310e6
dimashadi <dimashadianto460@gmail.com>
2024-06-27 22:29:14 +0700

    ubah tampilan like

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,12 +30,18 @@
                                         @csrf
                                         @if ($post->likedByUser(auth()->user()))
                                             @method('DELETE')
-                                            <button type="submit" class="bg-red-500 hover:bg-red-700 text-white py-1 px-3 rounded">
-                                                Unlike ({{ $post->likes_count }})
+                                            <button type="submit" class="text-red-500 hover:text-red-700 rounded">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill inline" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
+                                                </svg>
+                                                <span class="text-xs">{{ $post->likes_count }}</span>
                                             </button>
                                         @else
-                                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-3 rounded">
-                                                Like ({{ $post->likes_count }})
+                                            <button type="submit" class="text-gray-500 hover:text-gray-700 rounded">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart inline" viewBox="0 0 16 16">
+                                                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                                                </svg>
+                                                <span class="text-xs">{{ $post->likes_count }}</span>
                                             </button>
                                         @endif
                                     </form>
